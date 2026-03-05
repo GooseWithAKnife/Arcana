7d69bd7779aed1d07c200af2d5f5b28e523d253b
Ryan <ryan.rbsr@gmail.com>
2026-03-06 00:39:17 +0100

    unnecessary print

--- lua/arcana/mana_crystals.lua
+++ lua/arcana/mana_crystals.lua
@@ -421,7 +421,6 @@ if SERVER then
 	-- React to successful spell casts without requiring core.lua to know our API.
 	hook.Add("Arcana_SpellCastSucceeded", "ManaCrystals_ReportMagicUse", function(ply, spellId, ctxPos, reportContext)
 		if Arcana.ManaCrystals and Arcana.ManaCrystals.ReportMagicUse then
-			print("ReportMagicUse", ply, spellId, ctxPos, reportContext)
 			Arcana.ManaCrystals:ReportMagicUse(ply, ctxPos, spellId, reportContext)
 		end
 	end)
